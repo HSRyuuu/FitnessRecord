7da16a61d9e7b010971e94817066885110b6a61e
HyunSik Ryu <hsryu123@naver.com>
2023-10-19 22:46:29 +0900

    feat : securityConfiguration-> WebSecurityConfigurerAdapter 권장하지 않음에 따른 config 클래스 수정

--- src/main/java/com/example/fitnessrecord/global/auth/sercurity/config/SecurityConfiguration.java
+++ src/main/java/com/example/fitnessrecord/global/auth/sercurity/config/SecurityConfiguration.java
@@ -8,70 +8,63 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Slf4j
-@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityConfiguration {
 
-  private final Environment environment;
   private final AuthenticationFilter authenticationFilter;
   private final JwtExceptionFilter jwtExceptionFilter;
 
   private final MyAccessDeniedHandler myAccessDeniedHandler;
   private final MyAuthenticationEntryPoint myAuthenticationEntryPoint;
 
-  @Override
-  public void configure(HttpSecurity http) throws Exception {
-
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
         .httpBasic().disable()
         .csrf().disable()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
-          .authorizeRequests()
-          .antMatchers("/admin/**")
-          .hasAuthority("ROLE_ADMIN")
+        .authorizeRequests()
+        .antMatchers("/admin/**")
+        .hasAuthority("ROLE_ADMIN")
         .and()
-          .authorizeRequests()
-          .antMatchers("/user/**")
-          .hasAuthority("ROLE_USER")
+        .authorizeRequests()
+        .antMatchers("/user/**")
+        .hasAuthority("ROLE_USER")
         .and()
-          .exceptionHandling()
-            .authenticationEntryPoint(myAuthenticationEntryPoint)
-            .accessDeniedHandler(myAccessDeniedHandler)
+        .exceptionHandling()
+        .authenticationEntryPoint(myAuthenticationEntryPoint)
+        .accessDeniedHandler(myAccessDeniedHandler)
         .and()
-            .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
-            .addFilterBefore(jwtExceptionFilter, AuthenticationFilter.class);
+        .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        .addFilterBefore(jwtExceptionFilter, AuthenticationFilter.class);
 
-//    // 개발용
-//    if (Arrays.asList(environment.getActiveProfiles()).contains("dev")) {
-//      http.authorizeRequests().antMatchers("/**").permitAll();
-//    }
-
-  }
-
-  @Override
-  public void configure(final WebSecurity webSecurity) {
-    webSecurity.ignoring()
-        .antMatchers("/register/**", "/login/**", "/exception/**");
+    return http.build();
   }
 
   @Bean
-  @Override
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    return web -> web.ignoring().antMatchers("/register/**", "/login/**", "/exception/**");
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
   }
 }

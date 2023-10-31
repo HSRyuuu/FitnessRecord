e166970b833b13919c64882e71add0d3d38cc094
HyunSik Ryu <hsryu123@naver.com>
2023-10-31 23:40:15 +0900

    Refactor: @ManyToOne에서 즉시로딩이 필요없는 분 FetchType.LAZY로 설정

--- src/main/java/com/example/fitnessrecord/domain/record/trainingrecord/persist/TrainingRecord.java
+++ src/main/java/com/example/fitnessrecord/domain/record/trainingrecord/persist/TrainingRecord.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -31,7 +32,7 @@ public class TrainingRecord {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "USER_ID")
   private User user;
 
@@ -39,7 +40,9 @@ public class TrainingRecord {
 
   private boolean volumeSavedYn;
 
-  @OneToMany(mappedBy = "trainingRecord", cascade = CascadeType.REMOVE)
+  @OneToMany(mappedBy = "trainingRecord",
+      fetch = FetchType.LAZY,
+      cascade = CascadeType.REMOVE)
   private List<SetRecord> setRecords = new ArrayList<>();
 
   @Builder

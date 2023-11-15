30b407838be58b57346d8c8999482330d7b9edc7
HyunSik Ryu <hsryu123@naver.com>
2023-11-15 15:51:14 +0900

    Refactor: 루틴 공유 게시글 조회 기능을 BoardController로 옮김

--- src/main/java/com/example/fitnessrecord/community/routinepost/controller/RoutinePostController.java
+++ src/main/java/com/example/fitnessrecord/community/routinepost/controller/RoutinePostController.java
@@ -1,7 +1,6 @@
 package com.example.fitnessrecord.community.routinepost.controller;
 
 import com.example.fitnessrecord.community.routinepost.dto.AddRoutinePostInput;
-import com.example.fitnessrecord.community.routinepost.dto.RoutinePostDto;
 import com.example.fitnessrecord.community.routinepost.dto.RoutinePostResult;
 import com.example.fitnessrecord.community.routinepost.service.RoutinePostService;
 import com.example.fitnessrecord.global.auth.sercurity.principal.PrincipalDetails;
@@ -10,8 +9,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,16 +25,11 @@ public class RoutinePostController {
   @ApiOperation("루틴 공유 글을 작성한다.")
   @PostMapping("/post")
   public ResponseEntity<?> addRoutinePost(@RequestBody AddRoutinePostInput input,
-      @AuthenticationPrincipal PrincipalDetails principalDetails){
+      @AuthenticationPrincipal PrincipalDetails principalDetails) {
     RoutinePostResult result =
         routinePostService.addRoutinePost(input, principalDetails.getUserId());
     return ResponseEntity.ok(result);
   }
 
-  @ApiOperation("루틴 공유 글을 조회한다.")
-  @GetMapping("/post/{id}")
-  public ResponseEntity<?> getRoutinePost(@PathVariable Long id){
-    RoutinePostDto routinePost = routinePostService.getRoutinePost(id);
-    return ResponseEntity.ok(routinePost);
-  }
+
 }

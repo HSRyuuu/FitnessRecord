65431071c1170e809adf77c40469d65161710388
HyunSik Ryu <hsryu123@naver.com>
2023-11-15 15:51:14 +0900

    Refactor: 루틴 공유 게시글 조회 기능을 BoardController로 옮김

--- src/main/java/com/example/fitnessrecord/community/board/controller/BoardController.java
+++ src/main/java/com/example/fitnessrecord/community/board/controller/BoardController.java
@@ -2,10 +2,15 @@ package com.example.fitnessrecord.community.board.controller;
 
 import com.example.fitnessrecord.community.board.dto.BoardMainDto;
 import com.example.fitnessrecord.community.board.service.BoardService;
+import com.example.fitnessrecord.community.routinepost.dto.RoutinePostDto;
+import com.example.fitnessrecord.community.routinepost.service.RoutinePostService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
 
   private final BoardService boardService;
+  private final RoutinePostService routinePostService;
 
+  @ApiOperation("루틴 공유 게시판 Main 화면")
   @GetMapping("/main")
-  public ResponseEntity<?> board(@RequestParam(value = "p", defaultValue = "1")Integer page){
+  public ResponseEntity<?> board(@RequestParam(value = "p", defaultValue = "1") Integer page) {
     BoardMainDto boardMainData = boardService.getBoardMainData(page);
     return ResponseEntity.ok(boardMainData);
   }
+
+  @ApiOperation("루틴 공유 글을 조회한다.")
+  @GetMapping("/post/{id}")
+  public ResponseEntity<?> getRoutinePost(@PathVariable Long id) {
+    RoutinePostDto routinePost = routinePostService.getRoutinePost(id);
+    return ResponseEntity.ok(routinePost);
+  }
+
 }
